24fc4228c779174880200a78d1c6798b26c0a19f
MananKanani5 <kanani.manan5@gmail.com>
2024-07-25 00:52:10 +0530

    connection.js update

--- connection.js
+++ connection.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2');
 
 let connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: "Manan@210502",
-    database: "quora"
+    host: 'b4fns07m2f4ifunp9ira-mysql.services.clever-cloud.com',
+    user: 'u3qecqaodrblskes',
+    password: "xSC9WIi19dPvgfDfIJv6",
+    database: "b4fns07m2f4ifunp9ira",
+    port: 3306
 });
 
 module.exports = connection;
