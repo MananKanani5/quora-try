43b2511dce6e3ada3381a98809ef7cabf27a1dc2
MananKanani5 <kanani.manan5@gmail.com>
2024-07-25 01:18:25 +0530

    connection updated

--- connection.js
+++ connection.js
@@ -10,12 +10,12 @@ const connection = mysql.createConnection({
     port: process.env.MYSQL_ADDON_PORT
 });
 
-connection.connect(err => {
-    if (err) {
-        console.error('Database connection error:', err);
-        return;
-    }
-    console.log('Connected to MySQL database');
-});
+// connection.connect(err => {
+//     if (err) {
+//         console.error('Database connection error:', err);
+//         return;
+//     }
+//     console.log('Connected to MySQL database');
+// });
 
 module.exports = connection;
